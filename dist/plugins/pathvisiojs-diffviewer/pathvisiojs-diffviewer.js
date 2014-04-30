c361502b40c2810c67d381ba8411e3c9f3deb6b8
bumbu <bmbalex@gmail.com>
2014-04-30 23:54:15 +0300

    prototype diffviewer init

--- dist/plugins/pathvisiojs-diffviewer/pathvisiojs-diffviewer.js
+++ dist/plugins/pathvisiojs-diffviewer/pathvisiojs-diffviewer.js
@@ -1,6 +1,8 @@
 (function(window){
 
-  var optionsDefault = {}
+  var optionsDefault = {
+    sourceData: []
+  }
 
   /**
    * Init plugin
@@ -9,6 +11,8 @@
    */
   function PathvisiojsDiffviewer(pvjs, options) {
     var options = options || {}
+      , diffviewer = document.createElement('div')
+      , $diffviewer = d3.select(diffviewer)
 
     // Copy default options
     for (var i in optionsDefault) {
@@ -17,6 +21,31 @@
       }
     }
 
+    // Add diffviewer container before pvjs element
+    pvjs.$element[0][0].parentNode.insertBefore(diffviewer, pvjs.$element[0][0])
+
+    // Set diffviewer class
+    $diffviewer.attr('class', 'pathvisiojs-diffviewer')
+
+    // Create panes
+    $diffviewer.html('<div class="pane pane-left"></div><div class="pane pane-right"/></div>')
+
+    var $paneLeft = $diffviewer.select('.pane-left')
+      , $paneRight = $diffviewer.select('.pane-right')
+
+    // Move instance element into left pane
+    $paneLeft[0][0].appendChild(pvjs.$element[0][0])
+
+    // Create second instance container
+    var $pvjs2Element = $paneRight.append('div')
+
+    // pvjs options
+    var pvjsOptions = pvjs.getOptions()
+    pvjsOptions.sourceData = options.sourceData
+    pvjsOptions.manualRender = options.false
+
+    // Create second pvjs instance
+    var pvjs2 = window.pathvisiojs($pvjs2Element[0][0], pvjsOptions)[0]
   }
 
   /**
